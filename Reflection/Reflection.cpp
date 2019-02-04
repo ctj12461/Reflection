63c2299cf418a7c7fdd65d57ee40f47771171bf5
ctj <1246186412@qq.com>
2019-02-04 23:37:15 +0800

    Finish main parts of the project.

--- Reflection/Reflection.cpp
+++ Reflection/Reflection.cpp
@@ -5,8 +5,69 @@
 
 using namespace std;
 
-int main()
+Class ClassInfo = Class(sizeof(Object), "Object", "");
+
+Class& Object::getClass() const
 {
-	cout << "Hello CMake。" << endl;
-	return 0;
+	// TODO: 在此处插入 return 语句
 }
+
+Class::Class()
+	:
+	Size(0),
+	Ctor(nullptr)
+{
+}
+
+Class::Class(int size, std::string name, std::string baseName)
+	:
+	Size(size),
+	Name(name),
+	BaseName(baseName),
+	Ctor(nullptr)
+{
+	registerClass(*this);
+}
+
+std::string Class::getName() const{
+	return Name;
+}
+
+std::string Class::getBaseName() const{
+	return BaseName;
+}
+
+size_t Class::getSize() const{
+	return Size;
+}
+
+Class& Class::getBaseClass() const {
+	return forName(BaseName);
+}
+
+Object * Class::getInstance(){
+	return (*Ctor)();
+}
+
+Class Class::forName(std::string name){
+	return ClassMap[name];
+}
+
+bool Class::is(const Class& lhs, const Class& rhs){
+	if (lhs.Name == rhs.Name) {
+		return true;
+	} else if (lhs.Name == "") {
+		return false;
+	} else {
+		return is(lhs, forName(rhs.BaseName));
+	}
+}
+
+void Class::registerClass(const Class& classInfo){
+	ClassMap[classInfo.Name] = classInfo;
+}
+
+void Class::unregisterClass(const Class& classInfo){
+	ClassMap.erase(classInfo.Name);
+}
+
--- Reflection/Reflection.h
+++ Reflection/Reflection.h
@@ -1,8 +1,52 @@
 ﻿// Reflection.h: 标准系统包含文件的包含文件，
 // 或特定于项目的包含文件。
 
-#pragma once
+#ifndef REFLECTION_H
+#define RELFECTION_H
 
-#include <iostream>
+#include <string>
+#include <map>
+
+class Class;
+
+struct Object
+{
+	static Class ClassInfo;
+	virtual Object *create() = 0;
+	Class& getClass() const;
+};
+
+class Class
+{
+public:
+	using Contructor = Object * (Object::*)();
+	Class();
+	Class(int size, std::string name, std::string baseName);
+	Class& operator=(const Class& other) = default;
+
+	std::string getName() const;
+	std::string getBaseName() const;
+	size_t getSize() const;
+	Class& getBaseClass() const;
+	Object* getInstance();
+	static Class forName(std::string name);
+	static bool is(const Class& lhs, const Class& rhs);
+private:
+	std::string Name;
+	std::string BaseName;
+	size_t Size;
+	Contructor Ctor;
+	static std::map<std::string, Class> ClassMap;
+	static void registerClass(const Class& classInfo);
+	static void unregisterClass(const Class& classInfo);
+
+};
+
+#define SIZE(T) sizeof(T)
+#define NAME(N) #N
+#define runtime_dec(CLASS, BASE) static Class ClassInfo; = Class(CLASS, NAME(CLASS), NAME(BASE));
+#define runtime_dec(CLASS, BASE) Class ClassInfo = Class(CLASS, NAME(CLASS), NAME(BASE));
+
+#endif // !REFLECTION_H
 
 // TODO: 在此处引用程序需要的其他标头。
